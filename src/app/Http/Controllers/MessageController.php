b00e70ee47a71e787be56a83b5b4086007a957db
fazzty <stratbeat@gmail.com>
2024-04-30 20:43:59 +0900

    mvc post

--- src/app/Http/Controllers/MessageController.php
+++ src/app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use App\Models\Message;
 
@@ -12,5 +13,14 @@ class MessageController extends Controller
     {
         $messages = Message::all();
         return view('message/index', ['messages' => $messages]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $message = new Message();
+        $message->body = $request->body;
+        $message->save();
+
+        return redirect('/messages');
     }
 }
